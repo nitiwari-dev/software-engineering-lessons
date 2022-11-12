3e58b4226bd573ce6540e495853881bd467665a8
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-13 04:12:29 +0530

    Change LoginFlowTest with Http code

--- tdd/src/main/java/com/coderconsole/tdd/auth/LoginFlow.java
+++ tdd/src/main/java/com/coderconsole/tdd/auth/LoginFlow.java
@@ -6,8 +6,11 @@ public class LoginFlow {
         if (mobile.length() < 10 || !(mobile.startsWith("6")
                 || mobile.startsWith("7") || mobile.startsWith("8")
                 || mobile.startsWith("9"))
-        ) return 400;
+        ) return HTTP_INVALID_INPUT_400;
 
-        return 200;
+        return HTTP_OK_200;
     }
+
+    private static final int HTTP_OK_200 = 200;
+    private static final int HTTP_INVALID_INPUT_400 = 400;
 }
