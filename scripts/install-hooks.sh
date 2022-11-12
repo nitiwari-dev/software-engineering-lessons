5aeaa85ab5f5f03bca2bbab02b140637af34dbc0
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-13 05:19:45 +0530

    Update install-hook

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -3,7 +3,6 @@
 # Find the git directory
 GIT_DIR=$(git rev-parse --git-dir)
 
-# Create symlink
 #ln -sf scripts/pre-commit.sh "$GIT_DIR"/hooks/pre-commit
 cp scripts/pre-commit.sh "$GIT_DIR"/hooks/pre-commit
 
