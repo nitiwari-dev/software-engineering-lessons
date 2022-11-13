a184bbf41758bf9d2baf8323135131f19b632ad2
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-13 05:30:07 +0530

    Update install-hook

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -3,9 +3,9 @@
 # Find the git directory
 GIT_DIR=$(git rev-parse --git-dir)
 
-cp scripts/pre-commit.sh "$GIT_DIR"/hooks/pre-commit
+cp scripts/pre-commit.sh "$GIT_DIR"/hooks/
 chmod +x "$GIT_DIR"/hooks/pre-commit
-ln -sf scripts/pre-commit.sh "$GIT_DIR"/hooks/pre-commit
+#ln -sf scripts/pre-commit.sh "$GIT_DIR"/hooks/pre-commit
 
 echo "Pre commit hook installed!"
 
