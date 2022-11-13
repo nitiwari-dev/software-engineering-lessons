3d07e91b96ff4692333595019a993bd8b9afe366
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-13 05:58:38 +0530

    Update install hooks

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,4 +3,4 @@
 ## run tdd tests
 cd tdd || { echo "Cannot find path /tdd"; exit 1;}
 mvn test
-cd back || { echo "Cannot go back from /tdd"; exit 1;}
+cd .. || { echo "Cannot go back from /tdd"; exit 1;}
