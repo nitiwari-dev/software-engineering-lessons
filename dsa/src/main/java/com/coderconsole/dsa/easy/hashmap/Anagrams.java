841f32dfbd62cdea06a36ae661ec051678dcf7d0
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-19 20:27:06 +0530

    DSA | Easy | Anagram with sorting

--- dsa/src/main/java/com/coderconsole/dsa/easy/hashmap/Anagrams.java
+++ dsa/src/main/java/com/coderconsole/dsa/easy/hashmap/Anagrams.java
@@ -2,38 +2,60 @@ package com.coderconsole.dsa.easy.hashmap;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-
- An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
- Example 1:
-
- Input: strs = ["eat","tea","tan","ate","nat","bat"]
- Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
- Example 2:
-
- Input: strs = [""]
- Output: [[""]]
- Example 3:
-
- Input: strs = ["a"]
- Output: [["a"]]
-
-
- Approach 1 using: Sorting
- T: O(N*MLogM)
- S: O(N*M) -> N is number of elements in the array and M is the maximum length of string in array
-
- Approach 2 using: Character count
- T: O(N*M)
- S: O(N*M) -> N is number of elements in the array and M is the maximum length of string in array
-*/
+ * <p>
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * Example 2:
+ * <p>
+ * Input: strs = [""]
+ * Output: [[""]]
+ * Example 3:
+ * <p>
+ * Input: strs = ["a"]
+ * Output: [["a"]]
+ * <p>
+ * <p>
+ * Approach 1 using: Sorting
+ * T: O(N*MLogM)
+ * S: O(N*M) -> N is number of elements in the array and M is the maximum length of string in array
+ * <p>
+ * Approach 2 using: Character count
+ * T: O(N*M)
+ * S: O(N*M) -> N is number of elements in the array and M is the maximum length of string in array
+ */
 
 public class Anagrams {
     public List<List<String>> groupAnagramTogetherWithSorting(List<String> input) {
-        return List.of(input);
+
+        var map = new HashMap<String, List<String>>();
+        for (String originalKey : input) {
+            String sortedKey = getSortedKey(originalKey);
+            if (map.containsKey(sortedKey)) {
+                var group = map.get(sortedKey);
+                group.add(originalKey);
+                map.put(sortedKey, group);
+            } else {
+                var group = new ArrayList<String>();
+                group.add(originalKey);
+                map.put(sortedKey, group);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    private String getSortedKey(String originalKey) {
+        var chArray = originalKey.toCharArray();
+        Arrays.sort(chArray);
+        return new String(chArray);
     }
 }
--- dsa/src/test/java/com/coderconsole/dsa/easy/hashmap/AnagramsTest.java
+++ dsa/src/test/java/com/coderconsole/dsa/easy/hashmap/AnagramsTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AnagramsTest {
 
-    Anagrams anagrams;
+    private Anagrams anagrams;
 
     @BeforeEach
     void setUp() {
@@ -19,11 +20,21 @@ class AnagramsTest {
 
 
     @Test
-    @DisplayName("Given list of anagrams words group them together and return list of each group")
-    void returnGroupOfAnagramsUsingSorting() {
-        var input = List.of("eat","tea","tan","ate","nat","bat");
+    @DisplayName("Given empty list of anagrams return empty list")
+    void groupAnagram_emptyAnagramList_returnEmpty() {
+        var input = new ArrayList<String>();
         var output = anagrams.groupAnagramTogetherWithSorting(input);
-        assertThat(output).isNotEmpty();
+        assertThat(output).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Given list of anagrams words group them together and return list of each group")
+    void groupAnagram_listOfAnagrams_groupAnagramsUsingSorting() {
+        var input = List.of("eat", "tea", "tan", "ate", "nat", "bat");
+        var groups = List.of(List.of("eat", "tea", "ate"), List.of("tan", "nat"), List.of("bat"));
+        var output = anagrams.groupAnagramTogetherWithSorting(input);
+
+        assertThat(output).hasSize(groups.size()).hasSameElementsAs(groups);
     }
 
 }
