709254cd6a9710c5791df8fa3e3857d24c0bc681
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-22 20:42:36 +0530

    Change failure message

--- scripts/run-branch-name-check.sh
+++ scripts/run-branch-name-check.sh
@@ -3,11 +3,13 @@ echo "  Check local branch name..."
 
 local_branch=$(git rev-parse --abbrev-ref HEAD)
 accepted_branch="^((hotfix|conflict|bumpversion|revert|bug|fix|release|doc)(\/|-)[A-Za-z0-9._-]+$)"
-
+echo
 #check if the branch matching accepted branch pattern
 if [[ ! $local_branch =~ $accepted_branch ]];
 then
-  echo "Branch name should start with one below accepted format in regex!"
+  echo "Branch name \` $local_branch \` is not accepted format in regex!"
   echo "$accepted_branch"
   exit 1
 fi
+
+exit 0
