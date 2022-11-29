9cbdca67da1fb31512555901a80d3c7181bc01a6
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-11-30 01:39:43 +0530

    fun with golang

--- code/fun-with-golang/Main.go
+++ code/fun-with-golang/Main.go
@@ -1,13 +1,1 @@
-package main
-
-import (
-	"fun-with-golang/basic"
-	"fun-with-golang/hard"
-	"fun-with-golang/medium"
-)
-
-func main() {
-	basic.Init()
-	medium.Init()
-	hard.Init()
-}
+package fun_with_golang
--- code/fun-with-golang/helper/helper.go
+++ code/fun-with-golang/helper/helper.go
@@ -1,11 +1,1 @@
 package helper
-
-import "fmt"
-
-func Println(a ...interface{}) {
-	fmt.Println(a)
-}
-
-func Printf(format string, a ...interface{}) string {
-	return fmt.Sprintf(format, a)
-}
