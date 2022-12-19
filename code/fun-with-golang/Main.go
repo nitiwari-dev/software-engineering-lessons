310ad6353ab15664fbd746186569d30ea0e02cf4
Nitesh Tiwari <nitiwari.dev@gmail.com>
2022-12-19 22:39:57 +0530

    Fun with golang (#75)
    * Lession | fun with go | marshalling
    
    * Lession | fun with go | encoder at stdout

--- code/fun-with-golang/Main.go
+++ code/fun-with-golang/Main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fun-with-golang/basic"
+	"fun-with-golang/hard"
+	"fun-with-golang/medium"
 )
 
 func main() {
 	basic.Init()
-	//medium.Init()
-	//hard.Init()
+	medium.Init()
+	hard.Init()
 }
