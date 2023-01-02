e6e6d1c3ed361ac9496ab29148d763b56697a91d
Nitesh Tiwari <nitiwari.dev@gmail.com>
2023-01-02 23:38:50 +0530

    Fun with golang | medium

--- code/fun-with-golang/Main.go
+++ code/fun-with-golang/Main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fun-with-golang/basic"
-	"fun-with-golang/hard"
-	"fun-with-golang/medium"
 )
 
 func main() {
 	basic.Init()
-	medium.Init()
-	hard.Init()
+	//medium.Init()
+	//hard.Init()
 }
