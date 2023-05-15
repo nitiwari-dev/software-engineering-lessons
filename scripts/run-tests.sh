9beb012c847102ea51e824cbc2f79c64dc52e865
Nitesh Tiwari <nitiwari.dev@gmail.com>
2023-05-15 21:13:29 +0530

    agile | refactor to move tdd into agile repo

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 ## run tdd tests
-cd tdd || { echo "Cannot find path /tdd"; exit 1;}
+cd agile/tdd || { echo "Cannot find path agile/tdd"; exit 1;}
 mvn test || { echo "Test failed for 'tdd' project "; exit 1;}
-cd .. || { echo "Cannot go back from /tdd"; exit 1;}
+cd ../.. || { echo "Cannot go back from /tdd"; exit 1;}
 
 ## run dsa tests
 cd dsa-kotlin || { echo  "Cannot find path /dsa-kotlin"; exit 1;}
