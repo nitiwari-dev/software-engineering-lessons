a8c20a1a8d08ceae60a14086109676a6146611fd
Nitesh Tiwari <nitiwari.dev@gmail.com>
2023-08-19 13:14:42 +0530

    add sample for remaining timer (#116)

---
+++ code/fun-with-kotlin/src/main/kotlin/patterns/strategy/RemainingTimer.kt
@@ -0,0 +1,41 @@
+package patterns.strategy
+
+import java.util.concurrent.TimeUnit
+
+open class RemainingTimer(private val strategy: RemainingTimeStrategy) {
+
+    fun remainingTimer(inputMills: Long): String =  strategy.execute(inputMills)
+
+    fun interface RemainingTimeStrategy {
+        fun execute(timeInMillis: Long): String
+    }
+
+
+    class HoursMinutes: RemainingTimeStrategy {
+        override fun execute(timeInMillis: Long): String {
+            val toHours = TimeUnit.MILLISECONDS.toHours(timeInMillis)
+            val toMinutes = convertMillsToMinutes(timeInMillis, toHours)
+            return if (toHours == 0L) "$toMinutes min"
+            else if(toMinutes == 0L)"$toHours hr" else "$toHours hr $toMinutes min"
+        }
+
+        private fun convertMillsToMinutes(timeInMillis: Long, toHours: Long) =
+            TimeUnit.MILLISECONDS.toMinutes(timeInMillis - toHours * 60 * 60 * 1000)
+
+    }
+
+    class MinutesSeconds: RemainingTimeStrategy {
+        override fun execute(timeInMillis: Long): String {
+            val toMinutes = TimeUnit.MILLISECONDS.toMinutes(timeInMillis)
+
+            return if(toMinutes == 0L){
+                val toSeconds = TimeUnit.MILLISECONDS.toSeconds(timeInMillis)
+                "$toSeconds sec"
+            }else {
+                val toSeconds = TimeUnit.MILLISECONDS.toSeconds(timeInMillis - toMinutes * 60 * 1000)
+                if (toSeconds > 0) "$toMinutes min $toSeconds sec" else "$toMinutes min"
+            }
+        }
+
+    }
+}
