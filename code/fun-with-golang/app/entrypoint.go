530c337916b888c409433ae90eb7842475518617
Nitesh Tiwari <nitiwari.dev@gmail.com>
2024-08-03 02:45:35 +0530

    added the necessary tc for config

--- code/fun-with-golang/app/entrypoint.go
+++ code/fun-with-golang/app/entrypoint.go
@@ -4,12 +4,14 @@ import (
 	"fun-with-golang/config"
 	"fun-with-golang/intro"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
 func StartApp() {
 	appConfig, err := config.InitAppConfig()
 	if err != nil {
 		log.Err(err).Msgf("failed to init the app config")
+		os.Exit(1)
 	}
 
 	//Check to enable intro
