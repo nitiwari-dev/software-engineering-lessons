6b04f011f3836bc5f24e6e6f917680a2e71aaa26
Nitesh Tiwari <nitiwari.dev@gmail.com>
2024-11-10 16:16:37 +0530

    add partionTo customer

--- code/fun-with-kotlin/src/test/kotlin/generics/KBuildersKtTest.kt
+++ code/fun-with-kotlin/src/test/kotlin/generics/KBuildersKtTest.kt
@@ -1,4 +1,4 @@
-package builders
+package generics
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -20,5 +20,14 @@ class KBuildersKtTest {
         val actual = emptyMap<Int, String>()
         val expected = buildMap<Int, String> {}
         assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `given first and second choice return the partitioned output`(){
+        val input = listOf(1, 2, 3, 4, 5, 6)
+        val (even, odd) = input.partitionTo(mutableListOf(), mutableListOf()){ i -> i % 2 == 0 }
+
+        assertThat(even).isEqualTo(listOf(2, 4, 6))
+        assertThat(odd).isEqualTo(listOf(1, 3, 5))
     }
 }
