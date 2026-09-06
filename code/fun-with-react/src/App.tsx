4664ac4ef32ce7141da9996e56659313b4cd6f45
Nitesh Tiwari <nitiwari.dev@gmail.com>
2026-09-06 17:11:45 +0530

    add playerlist and new retry with generics in go 1.8 onwards

---
+++ code/fun-with-react/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PlayerList from './PlayerList'
+
+function App() {
+  return (
+      <section id="center">
+        <PlayerList/>
+      </section>
+  )
+}
+
+export default App
